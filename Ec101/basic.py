31e7f08cce22d6667500de020f636d772b06550a
Emil Colina <colinaleonemiljose@gmail.com>
2025-11-20 13:15:21 -0600

    ejemplo de sintaxis y semantica

---
+++ Ec101/basic.py
@@ -0,0 +1,7 @@
+# ejemplo de syntaxis básico en Python
+# print('hola')
+# print('mundo')
+
+# ejemplo de semantica básica en Python
+saludo = 'hola'
+print ('hola')
--- cap101/basic.py
+++
@@ -1,4 +0,0 @@
-name = 'Emil'
-lastName = 'Colina'
-
-print('hola, mi nombre es ' + name + ' ' + lastName)
