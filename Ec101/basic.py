45b0402f74ff4739f1fe5ee83a7299e35c21797d
Emil Colina <colinaleonemiljose@gmail.com>
2025-12-01 09:26:11 -0600

    ejempo de operaciones matematicas, archivo nuevo

--- Ec101/basic.py
+++ Ec101/basic.py
@@ -3,5 +3,26 @@
 # print('mundo')
 
 # ejemplo de semantica básica en Python
-saludo = 'hola'
-print ('hola')
+saludo = 'hola mi nombre es'
+nombre = 'Emil'
+apellido = 'Colina'
+edad = 28
+
+# print (saludo)
+# print (nombre)
+
+# uso de la función print y sus diferentes argumentos
+
+#print('hola', 'mundo,', 'estoy', 'probando', 'python')
+#print('hola' + 'mundo,'+ 'estoy' + 'probando' + 'print')
+
+# uso de parametros dentro de la función print
+#print('hola', 'mundo,', 'estoy', 'probando', 'python', sep='---')
+
+# print(saludo, nombre , apellido, end =f' y tengo {edad} años\n')
+
+print('{}: {} {} y tengo {} años'.format(saludo, nombre, apellido, edad))
+pi = 3.141592653589793
+print ('El valor de pi es {:.2f}'.format(pi))
+
+print('esto es un ejemplo de simbolos especiales: \n nueva línea \n\t tabulación \n\\ barra invertida \n\' comilla simple \n\" comilla doble')
---
+++ Ec101/operacionesMat.py
@@ -0,0 +1,12 @@
+#python manera una serie de operaciones matemáticas básicas
+A = 10
+B = 3
+
+Suma = A + B
+Resta = A - B
+Multiplicacion = A * B
+Division = A / B
+
+print(f'el resultado de las operaciones es:\nSuma: {Suma}\nResta: {Resta}\nMultiplicacion: {Multiplicacion}\nDivision: {Division}')
+
+#ademas de estas, existen otras operaciones matematicas en python
