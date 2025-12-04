48a68bc272ad3f1f1e24c18770ec98a9e81527af
Emil Colina <colinaleonemiljose@gmail.com>
2025-12-04 12:00:06 -0600

    Listas, creacion y usos basicos

--- Ec101/dataList.py
+++ Ec101/dataList.py
@@ -1,1 +1,19 @@
 #ejemplo de listas de datos
+conciertos2026 = [  'Too many zoo',
+                    'Rammstein',
+                    'Chet Faker',
+                    'The pillows']
+
+print(conciertos2026)
+
+numbers = [1,2,3,4, 'cinco']
+
+texto = 'hola todos, estoy aprendiendo python'
+
+mix = ['Emil', 1.69, 28, True, [1,2,3,4]]
+
+print(texto)
+print('El largo de la cadena es: ', len(texto))
+print('El primer elemento de la de lista mix es ', mix[0])
+print('El segundo elemento de la de lista mix es ', mix[1])
+print('El ultimo elemento de la de lista mix es ', mix[-1])
