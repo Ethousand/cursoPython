8392597cbf8138cb33bc1e02dce4c292c00f4f3a
Emil Colina <colinaleonemiljose@gmail.com>
2025-12-08 12:30:11 -0600

    metodos de listas, CRUD

--- Ec101/dataList.py
+++ Ec101/dataList.py
@@ -17,3 +17,21 @@ print('El largo de la cadena es: ', len(texto))
 print('El primer elemento de la de lista mix es ', mix[0])
 print('El segundo elemento de la de lista mix es ', mix[1])
 print('El ultimo elemento de la de lista mix es ', mix[-1])
+
+print(texto[1:10]) #lista desde el indice 1 hasta el 9
+print(texto[:5]) #lista desde el indice 0 hasta el 4
+print(mix[:]) #lista de todos los elementos 
+
+# metodos para modificar listas
+print('la lista de números al principio es: ', numbers)
+numbers.append(6) #agrega un elemento al final de la lista
+print(numbers)
+
+numbers.insert(1,15.3) #agrega un elemento en la posicion indicada
+print(numbers)
+
+numbers.remove('cinco') #elimina el elemento indicado
+numbers.pop() #elimina el ultimo elemento
+del numbers[0] #elimina el elemento en la posicion indicada
+
+print(numbers)
