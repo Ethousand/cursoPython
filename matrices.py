8597ff84e9086de4a6d7b33b9b5518d44a3fa5a6
Emil Colina <colinaleonemiljose@gmail.com>
2025-12-10 12:56:01 -0600

    presentacion de las listas y tuplas dentro de python

---
+++ matrices.py
@@ -0,0 +1,13 @@
+'''matriz = [[1, 2, 3],
+          [4, 5, 6],
+          [7, 8, 9]]
+
+print(matriz)
+print('dentro de una matriz podemos extraer filas', matriz[1], 'como datos puntuales', matriz[1][2])'''
+
+tupla = (1, 2, 3, 4, 5, 6, 7, 8, 9)
+print(tupla)
+
+print('tambien podemos extraer datos de una tupla, como por ejemplo el cuarto elemento:', tupla[3])
+
+tupla[0] = 10  # Esto generará un error porque las tuplas son inmutables
