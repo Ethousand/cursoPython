7ea2e17ba6596b9dab50dc39cc0c7e43a35e8f15
Emil Colina <colinaleonemiljose@gmail.com>
2025-12-16 12:25:54 -0600

    agregada logica de juego, tambien diccionario para simplificar el escrito de respuesta

--- RPS.py
+++ RPS.py
@@ -1,34 +1,22 @@
 import random
+
 Player = input('Enter your choice R for rock, P for paper, S for scissors: ').upper()
+Computer = random.choice(['R', 'P', 'S'])
 
+# Dictionary for showing full names
+RPS = {'R': 'Rock', 'P': 'Paper', 'S': 'Scissors'}
 
+# Validate player input
 while Player not in ['R', 'P', 'S']:
     Player = input('Invalid input! Please enter R for rock, P for paper, S for scissors: ').upper()
 
-Computer = random.choice(['R', 'P', 'S'])
-
-if Player == 'R':
-    if Computer == 'S':
-        print("Player wins! Rock crushes Scissors.")
-    elif Computer == 'R':
-        print("It's a tie! Both chose Rock.")
-
-elif Player == 'P':
-    if Computer == 'R':
-        print("Player wins! Paper covers Rock.")
-    elif Computer == 'P':
-        print("It's a tie! Both chose Paper.")
-
-elif Player == 'S':
-    if Computer == 'P':
-        print("Player wins! Scissors cut Paper.")
-    elif Computer == 'S':
-        print("It's a tie! Both chose Scissors.")
+# Im gonna use logic staments for to simplify the logic of the game
+# first its tie, second player win, last computer win
+if Player == Computer:
+    print(f'Both chose {Player}. It\'s a tie!')
+elif (Player == 'R' and Computer == 'S') or \
+     (Player == 'P' and Computer == 'R') or \
+     (Player == 'S' and Computer == 'P'):
+        print(f"Player wins! {RPS[Player]} beats {RPS[Computer]}")
 else:
-    
-
-
-Computer = random.choice(['R', 'P', 'S'])
-
-
-print(f"Player: {Player}, Computer: {Computer}")
+    print("Computer wins!", f"{RPS[Computer]} beats {RPS[Player]}")
