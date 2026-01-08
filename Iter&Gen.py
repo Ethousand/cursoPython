c9146a67cc0fb87f2d1bc1995b6ccdc16ce47629
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-08 12:50:42 -0600

    Final de clase bucles, inicio de iteradores y generadores

---
+++ Iter&Gen.py
@@ -0,0 +1,1 @@
+# iterador&generador
---
+++ bucles.py
@@ -0,0 +1,18 @@
+# inicio de bucles
+'''for i in range(5):
+    print('iteracion numero', i)
+
+fruits = ['manzana', 'banana', 'Naranja', 'pera', 'fresa']
+
+for fruit in fruits:
+    print('la fruta es:', fruit)
+'''
+x = 0
+while x < 10:
+    
+    if x == 4:
+        x += 1
+        continue
+    print('el valor de x es:', x)
+    x += 1
+     
