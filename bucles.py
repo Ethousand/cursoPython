efbcc0137ce684377818972b1a1f49f58b47d81d
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-08 13:06:22 -0600

    final del ejercicio de fibonacci

--- bucles.py
+++ bucles.py
@@ -15,4 +15,18 @@ while x < 10:
         continue
     print('el valor de x es:', x)
     x += 1
-     
+
+# practica con la ffrecuencia fibonacci
+
+end = int(input('ingrese cauntos numeros de la secuencia de fibonacci desea ver: '))
+
+def fibonacci(n):
+    global sequence
+    sequence = []
+    a, b = 0, 1
+    for i in range (n):
+        sequence.append(a)
+        a, b = b, a + b
+
+fibonacci(end)
+print(f'la cadena de fibonacci hasta el numero {end} es: {sequence}')
