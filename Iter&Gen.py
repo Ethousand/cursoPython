d1945d1a8272bc9572ceaac1206225fba03a059c
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-09 13:20:46 -0600

    ejemplo de función generador

--- Iter&Gen.py
+++ Iter&Gen.py
@@ -15,3 +15,14 @@ speller = iter(greeting)
 for letter in speller:
     print(letter)
 
+# generador con función
+
+print ("Generador con función:")
+
+def my_generator():
+    yield 1
+    yield 2
+    yield 3
+
+for num in my_generator():
+    print(num)
