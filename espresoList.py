19b5d302dda691c58bf91a80fbf507712527005a
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-12 13:30:16 -0600

    agregado compression list con condicionales

--- espresoList.py
+++ espresoList.py
@@ -2,8 +2,14 @@
 squares = [x**2 for x in range(10)]
 print(squares)
 
+#convert Celsius to fahrenheit
 
 celcius = [0, 10, 20, 34.5]
 farenheit = [(temp*9/5) + 32 for temp in celcius]
 print(farenheit)
 
+# extracción de numeros pares
+
+limit = int(input("Enter the limit: "))
+evens = [x for x in range(limit+1) if x%2 == 0]
+print(evens)
