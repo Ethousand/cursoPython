8f39caae3a80c49fcc50ad6d99e8a859f46c3c32
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-14 12:41:29 -0600

    agregadas funciones basicas de la calculadora

--- Calculator.py
+++ Calculator.py
@@ -7,3 +7,24 @@ def greet(name, last_name=""):
 greet("Emil", "Colina")
 
 greet("Ana")
+
+#calculator.py
+
+def add(a, b):
+    return a + b
+def subtract(a, b):
+    return a - b
+def multiply(a, b):
+    return a * b    
+def divide(a, b):
+    if b == 0: 
+        return "Error: Division by zero is not allowed."
+    return a / b
+def exponential(a, b):
+    return a ** b
+def sqrt(a):
+    if a < 0:
+        return "Error: Cannot compute square root of a negative number."
+    return a ** 0.5
+
+option = input("Selcciones una opción (1.- suma, 2.- resta, 3.- multiplicar, 4.- dividir, 5.- exponential, 6.- sqrt 7.- salir): ")
