aa47a7076cf00dcdda351f072ff7a6259a59ac73
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-23 13:15:13 -0600

    ejemplo de uso de lambda para el uso de listas

--- lambda.py
+++ lambda.py
@@ -6,3 +6,10 @@ square = lambda x: x ** 2
 print(add(2, 3))     
 print(mult(7, 6))    
 print(square(5))    
+
+# uso de lmabda para listas
+
+numbers = [1, 2, 3, 4, 5]
+cube_numbers = list(map(lambda x: x ** 3, numbers))
+
+print(cube_numbers)
