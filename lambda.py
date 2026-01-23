c690edf3d3a2acf1532624ece4bc993ac6565307
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-23 13:29:24 -0600

    agregado uso común para modificar listas

--- lambda.py
+++ lambda.py
@@ -9,7 +9,12 @@ print(square(5))
 
 # uso de lmabda para listas
 
-numbers = [1, 2, 3, 4, 5]
+numbers = range(21)
 cube_numbers = list(map(lambda x: x ** 3, numbers))
 
 print(cube_numbers)
+
+# uso de lambda para filtrar listas
+oddsNumbers = list(filter(lambda x: x%2 != 0, numbers))
+
+print(oddsNumbers)
