4622f9936b904ba34fe37391398ce26156f67d7c
Emil Colina <colinaleonemiljose@gmail.com>
2026-01-30 12:59:14 -0600

    finalizada clase excepciones, comentario con la libreria de errores de python

--- 4_funcExcept/Exceptions.py
+++ 4_funcExcept/Exceptions.py
@@ -7,7 +7,19 @@ try:
     resultado = 100 / user
 except ZeroDivisionError:
     print("Error: No se puede dividir por cero.")
-except ValueError:
+except ValueError as e:
     print("Error: Debe ingresar un número válido.")
+    print(f"Error details: {e}")
 else:
     print(f"El resultado es: {resultado}")
+
+'''
+def print_exception_hierarchy(exception_class, indent=0):
+    print(' ' * indent + exception_class.__name__)
+    for subclass in exception_class.__subclasses__():
+        print_exception_hierarchy(subclass, indent + 4)
+
+print_exception_hierarchy(Exception)
+# colecciones de excepciones: https://docs.python.org/3/library/exceptions.html
+
+'''
