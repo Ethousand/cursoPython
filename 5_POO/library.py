c6282cdd3049c08f0144dab4bf7d206df44d539d
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-02 12:30:28 -0600

    agregado ejemplo practico de POO a traves de una biblioteca

---
+++ 5_POO/library.py
@@ -0,0 +1,4 @@
+# Ejemplo de biblioteca simple utilizando POO
+# es se divide en ters clases, libro, persona y biblioteca, la biblioteca gestiona tanto libros como personas
+# la persona puede tomar prestado y devolver libros de la biblioteca, el libro tiene atributos como titulo y autor
+
