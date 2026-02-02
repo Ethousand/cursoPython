b17a02deda631d50b78c8178e3789ca2519cb0eb
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-02 12:11:13 -0600

    agregado ejemplo de cuenta de banco

--- 5_POO/Class&Method.py
+++ 5_POO/Class&Method.py
@@ -1,5 +1,5 @@
 #Orientend Object Programming - POO
-
+'''
 class Person:
     def __init__(self, name, age):
         self.name = name
@@ -13,3 +13,35 @@ person2 = Person('Carli', 32)
 
 person1.greet()
 person2.greet()
+
+'''
+class BankAccount:
+    def __init (self, account_holder, balance):
+        self.account_holder = account_holder
+        self.balance = balance
+        self.is_active = True
+
+    def deposit(self, amount):
+        if self.is_active:
+            self.balance += amount
+            print(f'Se han depositado {amount}. Nuevo saldo: {self.balance}')
+        else:
+            print('La cuenta está suspendida. No se pueden realizar depósitos.')
+    
+    def withdraw(self, amount):
+        if self.is_active:
+            if amount <= self.balance:
+                self.balance -= amount
+                print(f'Se han retirado {amount}. Nuevo saldo: {self.balance}')
+            else:
+                print('Fondos insuficientes.')
+        else:
+            print('La cuenta está suspendida. No se pueden realizar retiros.')
+    
+    def suspend_account(self):
+        self.is_active = False
+        print(f'La cuenta de {self.account_holder} ha sido suspendida.')
+    
+    def activate_account(self):
+        self.is_active = True
+        print(f'La cuenta de {self.account_holder} ha sido activada.')
