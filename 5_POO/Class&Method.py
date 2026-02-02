4d6902ef3b5bd8e972121de524ffc60f0032981a
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-02 12:03:49 -0600

    agregadas personas de ejemplo y uso metodo saludar

--- 5_POO/Class&Method.py
+++ 5_POO/Class&Method.py
@@ -7,3 +7,9 @@ class Person:
 
     def greet(self):
         print(f'hola mi nombre es {self.name} y tengo {self.age} años')
+
+person1 = Person('Emil', 28)
+person2 = Person('Carli', 32)
+
+person1.greet()
+person2.greet()
