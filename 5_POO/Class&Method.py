d0bcbd9c4d2172a1d1552ada933dd534cfd71906
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-02 12:01:00 -0600

    agregada clase de persona

--- 5_POO/Class&Method.py
+++ 5_POO/Class&Method.py
@@ -1,1 +1,9 @@
 #Orientend Object Programming - POO
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def greet(self):
+        print(f'hola mi nombre es {self.name} y tengo {self.age} años')
