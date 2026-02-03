0459b8181384c1871900845f9434c108cf0419b3
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-03 12:47:24 -0600

    agregadas funciones para prestar y devolver un libro

--- 5_POO/library.py
+++ 5_POO/library.py
@@ -6,5 +6,16 @@ class Book:
     def __init__(self, title, author):
         self.title = title
         self.author = author
-        self.is_borrowed = False
+        self.available = True
+
+    def borrow(self, user, book):
+        if self.available:
+            self.available = False
+            print(f"El usuario {user} ha tomado prestado el libro {book.title}")
+        else:
+            print(f"El libro '{book.title}' no está disponible")
+
+    def return_book(self, user, book):
+        self.available = True
+        print(f"El usuario {user} ha devuelto el libro {book.title}")
 
