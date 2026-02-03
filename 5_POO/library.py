1522a1e48314d8a465d8dc5f616f51635bf3def6
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-03 12:20:45 -0600

    creada clase libro, preparando clase persona

--- 5_POO/library.py
+++ 5_POO/library.py
@@ -2,3 +2,9 @@
 # es se divide en ters clases, libro, persona y biblioteca, la biblioteca gestiona tanto libros como personas
 # la persona puede tomar prestado y devolver libros de la biblioteca, el libro tiene atributos como titulo y autor
 
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_borrowed = False
+
