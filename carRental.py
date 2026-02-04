6e23956390037446193907660569936b56097a92
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-04 12:37:26 -0600

    agregado ejercicio de clase, renta de autos

---
+++ carRental.py
@@ -0,0 +1,3 @@
+# Ejercicio de clase, crear una consesionaria de alquiler de coches
+# este debe tener una collecion de coches disponibles para alquilar, donde el usuario pueda ver los coches disponibles
+# clases principales: Car, Person, Store
