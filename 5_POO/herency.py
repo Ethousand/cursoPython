6004233c63244562505de7cd240b1f8104afe881
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-09 12:55:00 -0600

    agergado ejemplo de herencia de clases, creado archivo

---
+++ 5_POO/herency.py
@@ -0,0 +1,3 @@
+# ejemplo de herencia en python
+# se tomara como ejemplo de la clase anteorior sobre autos y consesionarios
+# el consesionario se expandira para ofrecer tanto autos como bicicletas y camiones 
