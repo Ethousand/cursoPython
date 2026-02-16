a64015bace96feb2a06d19c1a4b94bdec6c6d527
Emil Colina <colinaleonemiljose@gmail.com>
2026-02-16 11:52:36 -0600

    agergado ejemplo de básico de herencia

--- 5_POO/herency.py
+++ 5_POO/herency.py
@@ -1,3 +1,14 @@
 # ejemplo de herencia en python
 # se tomara como ejemplo de la clase anteorior sobre autos y consesionarios
 # el consesionario se expandira para ofrecer tanto autos como bicicletas y camiones 
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def greet(self):
+        print(f"Hello, my name is {self.name} and I am {self.age} years old.")
+
+person1 = Person("Carli", 28)
+person1.greet()
