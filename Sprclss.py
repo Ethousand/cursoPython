34828f9657ec3a01504ad28c0733500fb1657486
Emil Colina <colinaleonemiljose@gmail.com>
2026-03-02 13:17:29 -0600

    Creada subclase estudiante, agregado metodo de saludar propio

--- Sprclss.py
+++ Sprclss.py
@@ -5,3 +5,12 @@ class Person:
     
     def greet(self):
         print(f"Hola, mi nombre es {self.name} y tengo {self.age} años.")
+
+class Student(Person):
+    def __init__(self, name, age, student_id):
+        super().__init__(name, age)
+        self.student_id = student_id
+
+    def greet(self):
+        super().greet()
+        print(f"Y mi ID de estudiante es {self.student_id}.")
