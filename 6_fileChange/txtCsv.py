3908b77fc3e9258244fa91348e9df1a07ba1ed90
Emil Colina <colinaleonemiljose@gmail.com>
2026-04-06 13:28:30 -0500

    agregados metodos de escritura y anexado de texto para .txt

--- 6_fileChange/txtCsv.py
+++ 6_fileChange/txtCsv.py
@@ -1,3 +1,16 @@
-with open('6_fileChange/tale.txt', 'r') as file:
-    text = file.read()
+# lectura de archivos con el modo 'r' (read)
+"""
+with open('6_fileChange/tale.txt', 'r', encoding='utf-8') as file:
+    for lines in file:
+        print(lines.strip())
+"""
 
+# escritura de archivos con el modo 'w' (write)
+"""
+with open('6_fileChange/tale.txt', 'w', encoding='utf-8') as file:
+    file.write('\n\nTarea por Emil.\n')
+"""
+
+# anexar texto a un archivo con el modo 'a' (append)
+with open('6_fileChange/tale.txt', 'a', encoding='utf-8') as file:
+    file.write('\n\nTarea por Emil.\n')
