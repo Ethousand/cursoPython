5508a46cc8a77f8bd824432a1dd93c9ae5e94fef
Emil Colina <colinaleonemiljose@gmail.com>
2026-04-14 11:57:55 -0500

    tarea extra terminada, agregado metodo para contar lineas con ciclo for

--- 6_fileChange/txtCsv.py
+++ 6_fileChange/txtCsv.py
@@ -1,9 +1,13 @@
 # lectura de archivos con el modo 'r' (read)
-"""
+
+lines = []
 with open('6_fileChange/tale.txt', 'r', encoding='utf-8') as file:
-    for lines in file:
-        print(lines.strip())
-"""
+    for line in file:
+        lines.append(line.strip())
+        print(line.strip())
+        print(str(len(lines)) + " lineas leidas\n\n")
+
+print("el archivo tiene " + str(len(lines)) + " lineas en total.")
 
 # escritura de archivos con el modo 'w' (write)
 """
@@ -12,5 +16,7 @@ with open('6_fileChange/tale.txt', 'w', encoding='utf-8') as file:
 """
 
 # anexar texto a un archivo con el modo 'a' (append)
+'''
 with open('6_fileChange/tale.txt', 'a', encoding='utf-8') as file:
     file.write('\n\nTarea por Emil.\n')
+'''
