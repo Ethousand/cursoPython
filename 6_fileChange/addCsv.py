8499c072b31ec3eb2a91f80b6d9bb9e4585ab61a
Emil Colina <colinaleonemiljose@gmail.com>
2026-05-06 13:34:04 -0500

    agregada linea para agregar productos, arreglada ubicacion del archivo

--- 6_fileChange/addCsv.py
+++ 6_fileChange/addCsv.py
@@ -7,6 +7,7 @@ new_product = {
                 'precio': 29.99
                }
 
-with open('Data.csv', mode='a', newline='') as file:
-    csv_writer = csv. DictWriter(file, fieldnames=new_product.keys())
+with open('6_fileChange/Data.csv', mode='a', newline='') as file:
+    file.write('\n')  # Agrega una nueva línea antes de escribir el nuevo producto
+    csv_writer = csv.DictWriter(file, fieldnames = new_product.keys())
     csv_writer.writerow(new_product)
