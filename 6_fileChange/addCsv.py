ce5caa43bc2662f5df1c55590546ae3f98f20757
Emil Colina <colinaleonemiljose@gmail.com>
2026-05-08 12:46:59 -0500

    comentado de la función para agregar variables variabbles hacia el csv

--- 6_fileChange/addCsv.py
+++ 6_fileChange/addCsv.py
@@ -7,7 +7,9 @@ new_product = {
                 'precio': 29.99
                }
 
+'''
 with open('6_fileChange/Data.csv', mode='a', newline='') as file:
     file.write('\n')  # Agrega una nueva línea antes de escribir el nuevo producto
     csv_writer = csv.DictWriter(file, fieldnames = new_product.keys())
     csv_writer.writerow(new_product)
+'''
