b68b89cb6bc46b2c04f3a6e0eff546418f223d0c
Emil Colina <colinaleonemiljose@gmail.com>
2026-05-13 13:06:05 -0500

    agregada función para leer archivo y rutas tanto para archivos original como actualizado

--- 6_fileChange/addCsv.py
+++ 6_fileChange/addCsv.py
@@ -1,5 +1,9 @@
 import csv
 
+# paths for the CSV file
+data_Db = '6_fileChange/Data.csv'
+updated_Db = '6_fileChange/UpdatedData.csv'
+
 new_product = {
                 'nombre': 'wireless Charger',
                 'modelo': 'WC-100',
@@ -9,7 +13,11 @@ new_product = {
 
 '''
 with open('6_fileChange/Data.csv', mode='a', newline='') as file:
-    file.write('\n')  # Agrega una nueva línea antes de escribir el nuevo producto
+    # file.write('\n') Agrega una nueva línea antes de escribir el nuevo producto
     csv_writer = csv.DictWriter(file, fieldnames = new_product.keys())
     csv_writer.writerow(new_product)
 '''
+with open('6_fileChange/Data.csv', mode='r', newline='') as file:
+    csv_reader = csv.DictReader(file)
+    # Obtener los nombres de las columnas eistentes
+    fieldnames = csv_reader.fieldnames + ['total_value']
